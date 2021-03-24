d938026cd4418d60b3607b194d4401e387d635b3
Franklin Martins <gentilezaxd@gmail.com>
2021-03-24 18:04:19 -0400

    Finish Site

--- src/App.js
+++ src/App.js
@@ -1,24 +1,139 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { FaBiohazard, FaRecycle, FaWhatsapp } from 'react-icons/fa'
+import { GiChemicalDrop, GiRadioactive, GiHandBandage } from 'react-icons/gi'
+
+import { 
+  Container, 
+  Content, 
+  Resume,
+  Types,
+  Generators,
+  Wrapper,
+  Card,
+  Graphicscontainer,
+  Footer
+} from './styles'
+
+import GraphicOne from './assets/1.png';
+import GraphicTwo from './assets/2.png';
 
 function App() {
+  const generators = [
+    {id: 1, imgURL: 'https://www.flaticon.com/svg/vstatic/svg/1595/1595929.svg?token=exp=1616436738~hmac=220b1ecf79be6fce47a4b2dbcff25aef', 
+    title: 'Serviços de tatuagens', text: 'A manipulação dos resíduos perfurocortantes com agentes biológicos pode causar acidentes, levando à contaminação da população, caso os Resíduos de Serviços de Saúde (RSS)', 
+    font: 'https://www.servioeste.com.br/blog/como-estudios-de-tatuagem-devem-gerenciar-seus-residuos'},
+    {id: 2, imgURL: 'https://www.flaticon.com/svg/vstatic/svg/3111/3111381.svg?token=exp=1616436834~hmac=e285b1e2a44fed3f3f1d6ef06d6b0c6b', 
+    title: 'Serviços de veterinária', text: 'Assim como outros serviços de saúde, hospitais, clínicas veterinárias e pet shops também devem se atentar ao descarte de seu lixo. Isso é ser responsável com a sociedade e com o meio ambiente, já que alguns itens podem colocar em risco a saúde dos pets e da população em geral.',
+     font: 'https://www.revistapetcenter.com.br/administracao/lixo-do-pet-shop-e-clinica-veterinaria-como-descarta-lo-corretamente-aprenda/'},
+    {id: 3, imgURL: 'https://www.flaticon.com/svg/vstatic/svg/3209/3209174.svg?token=exp=1616436925~hmac=e955712268acfafea1b3c1c76f4b5e0a',
+    title: 'Insulina de dependentes', 
+    text: 'Na forma mais comum de descarte de seringas, lancetas, fitas reagentes e frascos de insulina, os usuários usaram garrafas PET, embalagem considerada inadequada pela Associação Americana de Diabetes (ADA).', 
+    font: 'https://portal.coren-sp.gov.br/noticias/veja-como-orientar-os-usuarios-de-insulina-para-o-descarte-adequado-de-residuos/'
+    }
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Content>
+        <header>
+          <div>
+            <h1>A importância de saber sobre resíduos de serviços de saúde</h1>
+            <p>A população
+desinformada despeja esses resíduos no lixo domiciliar sem o conhecimento dos
+males que essa atitude pode acarretar ao meio ambiente e a saúde pública.</p>
+          </div>
+          <img src="https://www.flaticon.com/svg/vstatic/svg/1534/1534219.svg?token=exp=1616423761~hmac=a179ba82b017cfb81d84217c57b5ee98" alt="https://www.flaticon.com/svg/vstatic/svg/1534/1534219.svg?token=exp=1616423761~hmac=a179ba82b017cfb81d84217c57b5ee98"/>
+        </header>
+        <Resume>
+          <h2>Por quê você deveria se informar sobre resíduos?</h2>
+          <p>
+            Os resíduos e rejeitos quando descartados de maneira incorreta causam
+  impactos negativos para a saúde.
+  Além disso, o lixo é potencial poluidor do solo, água e do ar. Desta forma, faz-se
+  necessário abordar a importância dos processos de descarte corretamente e
+  destacar quais os malefícios que eles podem causar.
+          </p>
+          <h2>Pessoas mais afetadas!</h2>
+          <p>A seguir, um vídeo sobre as pessoas que mais lidam com coleta resíduos.</p>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/s846GukzIX4" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </Resume>
+        <Types>
+          <h1>Tipos de Resíduos</h1>
+          <ul>
+            <li>
+              <div><FaBiohazard /><h2>Infectante</h2></div>
+              <article>Os itens a serem descartados geralmente são acondicionados em sacos brancos com o símbolo de risco padrão para materiais infectantes, podendo ou não ser esterilizados o que faria com que passassem por um micro-ondas ou pela incineração.
+
+              <a href="https://www.ecovallore.com.br/coleta-residuo-infectante#:~:text=COMO%20DEVE%20SER%20REALIZADA%20A%20COLETA%20DE%20RES%C3%8DDUO%20INFECTANTE&text=Os%20itens%20a%20serem%20descartados,micro%2Dondas%20ou%20pela%20incinera%C3%A7%C3%A3o.">Fonte</a>
+              </article>
+            </li>
+            <li>
+              <div><GiChemicalDrop /><h2>Químico</h2></div>
+              <article>Antes de serem descartados, os resíduos químicos precisam passar por um tratamento especializado, além de ser coletado periodicamente. Os que não podem ser recuperados, devem ser guardados em reservatórios próprios para o descarte.
+                Os resíduos químicos são classificados de duas maneiras, sólidos e líquidos.
+                <a href="https://www.inovarambiental.com.br/cuidados-com-residuos-quimicos/#:~:text=Antes%20de%20serem%20descartados%2C%20os,duas%20maneiras%2C%20s%C3%B3lidos%20e%20l%C3%ADquidos.">Fonte</a>
+                </article>
+            </li>
+            <li>
+              <div><GiRadioactive /><h2>Radioativo</h2></div>
+              <article>A primeira etapa no gerenciamento é a armazenagem não definitiva dos resíduos no próprio local de geração, enquanto aguardam a disposição final. Sendo de responsabilidade do gerador a armazenagem correta.
+
+              <a href="https://www.vgresiduos.com.br/blog/como-deve-ser-feito-o-descarte-de-residuo-radioativo-hospitalar/">Fonte</a>
+              </article>
+            </li>
+            <li>
+              <div><FaRecycle/><h2>Comum</h2></div>
+              <article>Os resíduos comuns (aqueles que não estão contaminados e não oferecem grandes riscos) podem ser reciclados, na maioria das vezes. Assim, eles precisam ser levados a centros de reciclagem ou a postos de coleta seletiva. 
+
+              <a href="https://blog.seguridade.com.br/aprenda-como-fazer-o-descarte-de-residuos-corretamente/#:~:text=Os%20res%C3%ADduos%20comuns%20(aqueles%20que,%C3%A9%20sempre%20a%20melhor%20alternativa.">Fonte</a>
+              </article>
+            </li>
+            <li>
+              <div><GiHandBandage /><h2>Perfuro-Cortante</h2></div>
+              <article>Os materiais perfurocortantes devem ser descartados separadamente, no local de sua geração, imediatamente após o uso ou necessidade de descarte, em recipientes de paredes rígidas, resistentes à punctura, ruptura e vazamento, resistentes ao processo de esterilização, com tampa, devidamente identificados com o símbolo internacional de risco biológico
+
+              <a href="http://www.fiocruz.br/biosseguranca/Bis/virtual%20tour/hipertextos/up1/descarte-residuos-grupo-e.htm#:~:text=Os%20materiais%20perfurocortantes%20devem%20ser,devidamente%20identificados%20com%20o%20s%C3%ADmbolo">Fonte</a>
+              </article>
+            </li>
+          </ul>
+        </Types>
+        <Generators>
+          <h2>Alguns geradores desses resíduos</h2>
+          <Wrapper>
+          {generators.map(generator =>{
+            return(
+              <Card key={generator.id}>
+                <img src={generator.imgURL} alt={generator.title}/>  
+                <article>
+                  <h2> {generator.title} </h2>
+                  <p> {generator.text} </p>
+                  <a href={generator.font}>Fonte</a >
+                </article>
+          </Card>
+            )
+          })}
+          </Wrapper>
+
+        </Generators>
+        <Graphicscontainer>
+          <h2>Alguns dados levantados sobre resíduos de saúde:</h2>
+          <img src={GraphicOne} alt="gráfico-01"/>
+          <img src={GraphicTwo} alt="gráfico-02"/>
+        </Graphicscontainer>
+      <Footer>
+        <h2>Ficou com alguma dúvida?</h2>
+        <strong>Entre em contato conosco!</strong>
+        <ul>
+          <li>
+            <a href="https://wa.me/5509295595172/?text=Estou%20com%20dúvidas%20sobre%20resíduos%20">
+            <FaWhatsapp />
+            <p>Whatsapp</p>
+            </a>
+          </li>
+        </ul>
+      </Footer>
+      </Content>
+    </Container>
   );
 }
 
---
+++ src/styles.js
@@ -0,0 +1,260 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    background-color: #e0e0e0;
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Content = styled.div`
+    align-self: flex-start;
+    display: flex;
+    margin-top: 20px;
+    padding: 20px;
+    flex-direction: column;
+    background-color: #fff;
+    width: 1200px;
+    justify-content: center;
+    
+
+    header{
+        
+        display: flex;
+        align-items: center;
+        justify-content: space-evenly;
+        h1{
+            color: #299610;
+            margin-bottom: 12px;
+        }
+        div{
+            width: 400px;
+        }
+    }
+    img{
+        width: 300px;
+    }
+
+@media screen and (max-width: 900px){
+    header{
+        h1{
+            font-size: 32px;
+        }
+        flex-direction: column;
+        img{
+            margin-top: 20px;
+            width: 200px;
+        }
+    }
+}
+`;
+
+export const Resume = styled.article`
+align-self: center;
+margin-top: 40px;
+width: 900px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+h2{
+    color: #299610;
+    font-size: 28px;
+    margin-top: 12px;
+    margin-bottom: 12px;
+}
+p{
+    margin-bottom: 12px;
+}
+@media screen and (max-width: 900px){
+    width: 90vw;
+    padding: 20px;
+    iframe{
+        width: 400px;
+        height: 300px;
+    }
+}
+`;
+
+export const Types = styled.div`
+background-color: #299610;
+margin-top: 90px;
+width: 1020px;
+border-radius: 4px;
+align-self: center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 10px;
+h1{
+    color: #fff;
+    margin-bottom: 20px;
+}
+ul{
+    display: flex;
+    justify-content: center;
+    align-self: center;
+}
+li{
+    a{
+        align-self: flex-end;
+        justify-self: flex-end;
+    }
+    div{
+        justify-self: flex-start;
+        align-self: flex-start;
+        padding: 10px;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 14px;
+    }
+    article{
+        padding: 2px;
+        margin-left: 15px;
+        color: #fff;
+        width: 180px;
+    }
+    svg{
+        width: 25px;
+        height: 25px;
+        padding: 8px;
+    }
+}
+li + li{
+    border-left: 4px solid #fff;
+}
+@media screen and (max-width: 900px){
+    h1{
+        font-size: 32px;
+    }
+    ul{
+        flex-direction: column;
+    }
+    li{
+        border-left: none;
+        margin-bottom: 25px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+    }
+    li+li{
+        border-left: none;
+        border-top: 4px solid #fff;
+    }
+}
+`;
+
+export const Generators = styled.article`
+margin-top: 40px;
+align-self: center;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+padding: 20px 20px 60px 20px;
+h2{
+    color: #299610;
+}
+`;
+
+export const Wrapper = styled.div`
+    display: flex;
+    overflow: auto;
+
+@media screen and (max-width: 900px){
+    flex-direction: column;
+}
+`;
+
+export const Card = styled.div`
+margin-top: 40px;
+margin: 10px;
+width: 300px;
+height: 400px;
+display: flex;
+flex-direction: column;
+justify-content: flex-start;
+align-items: center;
+padding: 20px;
+border: 3px solid #E0E0E0;
+border-radius: 8px;
+    img{
+        margin-left: 30%;
+        margin-bottom: 20px;
+        align-self: flex-start;
+        width: 100px;
+        color: #299610;
+    }
+    h2{
+        color:#299610;
+    }
+    a{
+        border-bottom: 2px solid #299610;
+    }
+    article{
+        width: 300px;
+        h2{
+            margin-bottom: 10px;
+        }
+    }
+`;
+
+export const Graphicscontainer = styled.div`
+align-self: center;
+justify-self: center;
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+    img{
+        border: 3px solid #E0E0E0;
+        width: 600px;
+        margin: 20px;
+    }
+    h2{
+        color: #299610;
+    }
+@media screen and (max-width: 700px){
+    width: 80vw;
+    img{
+        width: 450px;
+    }
+}
+`;
+
+export const Footer = styled.footer`
+margin-top: 100px;
+width: 100%;
+height: 200px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+background-color:  #299610;
+
+    ul{
+        display: flex;
+    }
+    li,a{
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
+    h1,h2,h3,p,strong{
+        color: #fff;
+        margin: 10px;
+    }
+    svg{
+        width: 35px;
+        height: 35px;
+        filter: invert();
+    }
+`;
